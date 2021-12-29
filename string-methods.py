7f86b23150ec5d09be13899cf30af93a2309c08f
Veysel ÖREN <orenveysel@outlook.com>
2021-12-29 21:55:25 +0300

    Create string-methods.py

---
+++ string-methods.py
@@ -0,0 +1,21 @@
+message = "Hello There. My name is Ebubekir Sıddık"
+
+# message = message.upper()
+# message = message.lower()
+# message = message.title()
+# message = message.capitalize()
+# message = message.strip()
+# message = message.split()
+# message = message.split('.')
+# message = '---'.join(message)
+# message = message.find('Ebubekir')
+# isFound = message.startswith('H')
+# isFound = message.endswith('k')
+# message = message.replace('Ebubekir','Übeyd')
+# message = message.replace('Ü','U').replace('ş','s').replace(' ','*')
+# message = message.center(50,'*')
+
+
+
+print(message)
+
